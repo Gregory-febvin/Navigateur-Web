baaa5dcf0444dda39cddca767aecb6a7f59cb3c6
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2024-09-27 20:38:56 +0200

    fix: restructuration + devtools windows + style

--- src/app/browser.service.ts
+++ src/app/browser.service.ts
@@ -1,54 +1,54 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BrowserService {
 
-  url = 'https://amiens.unilasalle.fr';
-  canGoBack = false;
-  canGoForward = false;
+    url = '';
+    canGoBack = false;
+    canGoForward = false;
 
-// @ts-ignore
-  electronAPI = window.electronAPI;
+    // @ts-ignore
+    electronAPI = window.electronAPI;
 
-  toogleDevTool() {
-    this.electronAPI.toogleDevTool();
-  }
+    toogleDevTool() {
+        this.electronAPI.toogleDevTool();
+    }
 
-  goBack() {
-    this.electronAPI.goBack();
-    this.updateHistory();
-  }
+    goBack() {
+        this.electronAPI.goBack();
+        this.updateHistory();
+    }
 
-  goForward() {
-    this.electronAPI.goForward();
-    this.updateHistory();
-  }
+    goForward() {
+        this.electronAPI.goForward();
+        this.updateHistory();
+    }
 
-  refresh() {
-    this.electronAPI.refresh();
-  }
+    refresh() {
+        this.electronAPI.refresh();
+    }
 
-  goToPage(url: string) {
-    this.electronAPI.goToPage(url)
-      .then(() => this.updateHistory());
-  }
+    goToPage(url: string) {
+        this.electronAPI.goToPage(url)
+            .then(() => this.updateHistory());
+    }
 
-  setToCurrentUrl() {
-    this.electronAPI.currentUrl()
-      .then((url :string) => {
-        this.url = url;
-      });
-  }
+    setToCurrentUrl() {
+        this.electronAPI.currentUrl()
+            .then((url: string) => {
+                this.url = url;
+            });
+    }
 
-  updateHistory() {
-    this.setToCurrentUrl();
+    updateHistory() {
+        this.setToCurrentUrl();
 
-    this.electronAPI.canGoBack()
-      .then((canGoBack : boolean) => this.canGoBack = canGoBack);
+        this.electronAPI.canGoBack()
+            .then((canGoBack: boolean) => this.canGoBack = canGoBack);
 
-    this.electronAPI.canGoForward()
-      .then((canGoForward : boolean) => this.canGoForward = canGoForward);
-  }
+        this.electronAPI.canGoForward()
+            .then((canGoForward: boolean) => this.canGoForward = canGoForward);
+    }
 }
